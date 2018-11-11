80b3c872c51aa6c6c9f6b7bfe555abe74fb82b7f
liangxun <liangxun21@gmail.com>
2018-11-11 21:52:39 +0800

    在解析页面时候添加try结构避免特殊情况

--- CNKI/CNKI/spiders/spiders.py
+++ CNKI/CNKI/spiders/spiders.py
@@ -19,14 +19,16 @@ class CNKISpider(scrapy.Spider):
         urls = [
             'http://yuanjian.cnki.net/Search/ListResult'
         ]
+        subjectCode = 'J159'   # 金融学
+        # subjectCode = 'F087'   # 喜剧电影与电脑技术
         formdata = {
             'searchType': 'MulityTermsSearch',
             'ParamIsNullOrEmpty': 'false',
             'Islegal': 'false',
-            'Subject': 'J159',
+            'Subject': subjectCode,
             'Order': '2',
             'Page': '4',
-            'ZtCode': 'J159',
+            'ZtCode': subjectCode,
         }
         for url in urls:
             for page in range(10):
@@ -44,28 +46,32 @@ class CNKISpider(scrapy.Spider):
                 yield scrapy.Request(url=href, callback=self.parse_news, meta={'title': title, 'url': href})
 
     def parse_paper(self, response):
+        """提取期刊、论文类型文章摘要"""
         if response.status is 200:
-            item = CnkiItem()
-            item['title'] = response.meta['title']
-            item['href'] = response.meta['url']
-            abstract = response.xpath(".//div[@style='text-align:left;word-break:break-all']/text()")[1].extract()
-            item['abstract'] = abstract.strip()
-            yield item
-        else:
-            print("response.status=%d" % response.status)
+            try:
+                item = CnkiItem()
+                item['title'] = response.meta['title']
+                item['href'] = response.meta['url']
+                abstract = response.xpath(".//div[@style='text-align:left;word-break:break-all']/text()")[1].extract()
+                item['abstract'] = abstract.strip()
+                yield item
+            finally:
+                pass
 
     def parse_news(self, response):
+        """提取新闻类型文章的摘要"""
         if response.status is 200:
-            item = CnkiItem()
-            item['title'] = response.meta['title']
-            item['href'] = response.meta['url']
-            abstract = response.xpath('//div[@class="intro"]/p/text()')[0].extract()
-            abstract = abstract.strip()
             try:
-                abstract = re.sub('\.\.\.\\xa0(.|\n)*\(本文共\d页\)', '', abstract)
+                item = CnkiItem()
+                item['title'] = response.meta['title']
+                item['href'] = response.meta['url']
+                abstract = response.xpath('//div[@class="intro"]/p/text()')[0].extract()
+                abstract = abstract.strip()
+                try:
+                    abstract = re.sub('\.\.\.\\xa0(.|\n)*\(本文共\d页\)', '', abstract)
+                finally:
+                    item['abstract'] = abstract.strip()
+                yield item
             finally:
-                item['abstract'] = abstract.strip()
-            yield item
-        else:
-            print("response.status=%d" % response.status)
+                pass
 
